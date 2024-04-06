ab44041532c5315af38ffa88dbfcae974dde3fc4
Sam Komarov <sam@skomarov.com>
2024-04-06 16:31:22 +0300

    move router to its own package

--- internal/frontend/bot.go
+++ internal/frontend/bot.go
@@ -1,17 +1,19 @@
 package frontend
 
 import (
+	"github.com/k0marov/newsbot/internal/frontend/router"
+	"github.com/k0marov/newsbot/internal/frontend/texts"
 	tele "gopkg.in/telebot.v3"
 	"log"
 	"time"
 )
 
-func StartBot(token string, svc Service) {
+func StartBot(token string, svc router.Service) {
 	pref := tele.Settings{
 		Token:  token,
 		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
 		OnError: func(err error, context tele.Context) {
-			context.Reply("Oops, some error happened!")
+			context.Reply(texts.UnknownError)
 			log.Println("ERROR:", err)
 		},
 	}
@@ -22,7 +24,7 @@ func StartBot(token string, svc Service) {
 		return
 	}
 
-	router := NewRouter(svc)
+	router := router.NewRouter(svc)
 
 	router.DefineRoutes(b)
 
--- internal/frontend/texts/texts.go
+++ internal/frontend/texts/texts.go
@@ -4,4 +4,5 @@ const (
 	Start                = "Старт"
 	CorrectPassEntered   = "Верный пароль"
 	IncorrectPassEntered = "Неверный пароль"
+	UnknownError         = "Ой, произошла ошибка, попробуйте позже."
 )
