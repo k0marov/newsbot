36970ae192ab06676149404368d08427e3ca32ca
Sam Komarov <sam@skomarov.com>
2024-04-25 22:40:07 +0300

    set page size to 10

--- internal/backend/datasources/newsapi/const.go
+++ internal/backend/datasources/newsapi/const.go
@@ -5,7 +5,7 @@ import "fmt"
 const (
 	searchURL   = "https://www.nl.go.kr/seoji/contents/S80100000000.do?schType=simple&ebookYn=Y#link"
 	searchQuery = "삽화가(그림작가)"
-	pageSize    = 500
+	pageSize    = 10
 )
 
 func constructNewsEntryURL(isbn string) string {
